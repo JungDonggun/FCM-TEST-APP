955f894debefed7d9bf2c221955761dbd7452494
JungDonggun <donggun.dev@gmail.com>
2020-04-02 17:11:14 +0900

    Initial Setting

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from "react-native"
+
+const App: React.FunctionComponent = () => {
+  return (
+      <View>
+        <Text>welcome to React native App</Text>
+      </View>
+  )
+}
+
+
+export default App;
