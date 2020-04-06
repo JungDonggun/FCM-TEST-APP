71a3682315d972e5d9c7203c64f170101297c25a
JungDonggun <donggun.dev@gmail.com>
2020-04-06 23:29:38 +0900

    Update Push Notification Setting

--- App.tsx
+++ App.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
-import { SafeAreaView, Text, View } from "react-native"
+import { SafeAreaView, Text, Platform } from "react-native"
+import messaging from '@react-native-firebase/messaging';
 
 const App: React.FunctionComponent = () => {
+
+  React.useEffect(() => {
+    checkApplicationPermission()
+
+    messaging().onMessage(async (remoteMessage) => {
+      console.log('FCM Message Data:', remoteMessage);
+    })
+  }, [])
+
+  const registerAppWithFCM = async () => {
+    const token = await messaging().registerDeviceForRemoteMessages();
+
+    console.log('Firebase Token', token)
+  }
+
+  const checkApplicationPermission = async () => {
+    try {
+      const settings = await messaging().requestPermission();
+
+      // settings only available on iOS
+      if (settings) {
+        console.log('User has notification permissions enabled');
+      } else {
+        console.log('User has notification permissions disabled');
+      }
+    } catch (error) {
+      // User has rejected permissions
+      console.log('Error', error)
+      alert("you can't handle push notification");
+    }
+  }
+
+
   return (
       <SafeAreaView>
         <Text>welcome to React native App</Text>
