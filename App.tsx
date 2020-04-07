b18f7b8c17be81b2f959c023df4f3b8be9626b47
JungDonggun <donggun.dev@gmail.com>
2020-04-07 14:24:06 +0900

    Update Push Notification Setting 2

--- App.tsx
+++ App.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import { SafeAreaView, Text, Platform } from "react-native"
+import { SafeAreaView, Text } from "react-native"
 import messaging from '@react-native-firebase/messaging';
 
 const App: React.FunctionComponent = () => {
 
   React.useEffect(() => {
+    registerAppWithFCM()
     checkApplicationPermission()
 
-    messaging().onMessage(async (remoteMessage) => {
-      console.log('FCM Message Data:', remoteMessage);
-    })
+    messaging().getInitialNotification()
+        .then(remoteMessage => {
+          if (remoteMessage) {
+            console.log(
+                'Notification caused app to open from quit state:',
+                remoteMessage.notification,
+            );
+          }
+        });
+
+
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage,
+      );
+    });
+
+
+    messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM Message arrived!', remoteMessage)
+    });
+
   }, [])
 
   const registerAppWithFCM = async () => {
